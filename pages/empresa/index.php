17d24f8e0029be646476d73229efc3dda72c336f
Raissa Almeida <rayssanegah713@gmail.com>
2023-09-05 14:14:07 -0300

    atualizacao 5.09

---
+++ pages/empresa/index.php
@@ -0,0 +1,24 @@
+<nav class="navbar sticky-top bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Lista de Empresas</a>
+  </div>
+</nav>
+
+
+
+<div class="container-fluid">
+    <table class="table"> 
+          <thead> 
+               <tr>
+                 <th scope="col">Nome da Empresa</th>
+                 <th scope="col">Telefone</th>
+               </tr> 
+          </thead>  
+          <tbody>
+             <tr>
+                <td><a href="#">ETEC Pedro Badran</a></td>
+                <td >(16)3818-2196</td>
+             </tr>
+          </tbody>  
+    </table>    
+</div>    
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,23 @@
 <link href="static/css/header.css" rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <header>
-    <nav>
-        <img src="/static/imagens/logo.png" class="logo">
-        <div id="titulo">CarreiraEtec</div>
-</nav>
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="static/imagens/logo.png" alt="logo" width="60" height="60"
+                    class="d-inline-block align-text-top">
+                CarreiraEtec
+            </a>
+
+
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="empresa">Empresas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
 </header>
